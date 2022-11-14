0eec163ecc16814cf9af44818a70b93c8d1a53fa
Nada Ezzdin <nadaeezzdin@gmail.com>
2022-11-14 14:36:55 +0200

    Refactor campgrounds

--- validateSchemas.js
+++ validateSchemas.js
@@ -32,11 +32,11 @@ const Joi = BaseJoi.extend(extension)
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
-      title: Joi.string().required().escapeHTML,
+      title: Joi.string().required().escapeHTML(),
       price: Joi.number().required().min(0),
       //image: Joi.string().required(),
-      location: Joi.string().required().escapeHTML,
-      description: Joi.string().required().escapeHTML,
+      location: Joi.string().required().escapeHTML(),
+      description: Joi.string().required().escapeHTML(),
     }).required(),
     deleteImages: Joi.array()
   });
@@ -46,6 +46,6 @@ module.exports.campgroundSchema = Joi.object({
   module.exports.reviewSchema = Joi.object({
     review: Joi.object({
       rating: Joi.number().required().min(1).max(5),
-      body: Joi.string().required().escapeHTML
+      body: Joi.string().required().escapeHTML()
     }).required()
   });
